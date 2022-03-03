6a66e0eb593673c7d5d0922d058350c7862262f8
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-03-03 18:28:27 +0530

    designing completed almost

--- src/RightBar.jsx
+++ src/RightBar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './rightbar.css'
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
 
 const RightBar = () => {
   return (
     <div className='rightbar-body'>
-      rightbar
+      <div className="brand">
+    <h1>WEATHER APP</h1>
+      </div>
+      <div className="celsius-container">
+        <div className="celsius">08゜</div>
+        <div className="place">
+          <h2>London</h2>
+          <p>6:09 sunday</p>
+        </div>
+        <div >
+          <WbSunnyIcon className="image"/>
+        </div>
+      </div> 
     </div>
   )
 }
